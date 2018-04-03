d7b028357c210f4377376d7ca0df14b85fb34eeb
IgnacioRondon <ignacio.rondon@gsdynamics.com>
2018-04-03 12:22:14 -0500

    Correlativo a Ventas y Prestamos segun el dia

--- nuevoPrestamo_DatosGenerales.php
+++ nuevoPrestamo_DatosGenerales.php
@@ -3,7 +3,21 @@ include('session.php');
 include('declaracionFechas.php');
 if(isset($_SESSION['login'])) {
     include('adminTemplateAutocomplete.php');
+    $aux1 = 0;
+    $fechaTransaccion = date("Y-m-d");
+    $result1 = mysqli_query($link,"SELECT idTransaccion FROM Transaccion WHERE fechaTransaccion LIKE '{$fechaTransaccion}%' AND idTipoTransaccion = '6'");
+    $numrow = mysqli_num_rows($result1);
+    if($numrow > 0){
+        while ($fila1 = mysqli_fetch_array($result1)){
+            $aux1++;
+        }
+    }else{
+        $aux1 = 1;
+    }
+    $aux1++;
+    
     $idPS = idgen("PS");
+    $idPS = $idPS."-".$aux1;
     ?>
 
     <section class="container">
